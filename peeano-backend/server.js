507bc8f29f6ee666894b768fb129bdaa47acf3cf
Alan Park <alansdpark@gmail.com>
2020-11-06 14:54:01 -0800

    Connected MongoDB database to server

--- peeano-backend/server.js
+++ peeano-backend/server.js
@@ -12,6 +12,36 @@ let Users = require('./users.model');
 app.use(cors());
 app.use(bodyParser.json());
 
+// MONGODB Atlas
+const {MongoClient} = require('mongodb');
+const uri = "mongodb+srv://alanparksjsu:peeanosjsu@cluster0.t2kms.mongodb.net/peeano?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+async function main() {
+	try {
+		await client.connect();
+		// await listDatabases(client);
+
+		console.log("PEEANO DATABASE Query: ");
+		const db = client.db("peeano");
+		const col = db.collection("users");
+
+		const myDoc = await col.findOne({"username":"test"});
+		console.log(myDoc);
+
+	} catch (e) {
+		console.error(e);
+	} finally {
+		await client.close();
+	}
+}
+main().catch(console.error);
+
+async function listDatabases(client) {
+	databasesList = await client.db().admin().listDatabases();
+	console.log("Databases: ");
+	databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+// END MONGODB Atlas
 
 mongoose.connect('mongodb://127.0.0.1:27017/peeano', { useNewUrlParser: true });
 const connection = mongoose.connection;
--- peeano-frontend/src/components/signup.js
+++ peeano-frontend/src/components/signup.js
@@ -4,6 +4,9 @@ export default function signup() {
     return (
         <div>
             <h1>Sign up</h1>
+            <form>
+
+            </form>
         </div>
     )
 }
