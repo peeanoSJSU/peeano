4c91c1b52d016bd5aee410d25cc66a93c18be74b
Isaaccmcd <isaac.mcdonald@sjsu.edu>
2020-11-13 01:30:54 -0800

    home page

---
+++ peeano-frontend/src/components/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+
+
+function Home() {
+    return (
+    	<div>
+			<div className='titleContainer'>
+				<h1 className='pageTitle'>Welcome to Peeano virtual piano</h1>
+			</div>
+		</div>
+	)
+}
+
+export default Home;
