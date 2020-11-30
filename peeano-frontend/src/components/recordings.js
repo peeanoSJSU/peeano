4c60b77898581689fd0fb7b22ca8b74e1adfc5b2
Alan Park <alansdpark@gmail.com>
2020-11-30 12:19:58 -0800

    added userData to recordings.js

--- peeano-frontend/src/components/recordings.js
+++ peeano-frontend/src/components/recordings.js
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import './style.css';
 import { Button } from './button';
+import UserContext from '../context/UserContext';
 
 function Recordings(){
 
 	const [click, setClick] = useState(false);
 	const handleClick = () => setClick(!click);
+
+	const {userData} = useContext(UserContext);
 
 	return(
 		<div className='linkBody'>
